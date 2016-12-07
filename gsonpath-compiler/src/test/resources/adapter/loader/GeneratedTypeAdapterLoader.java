01807d5c4f2aa4cc0bb6b1683fbc6655921bfbcb
LachlanMcKee <lachlandevelopment@gmail.com>
2016-12-07 23:24:37 +1100

    Issue #58 Fixed package local defect due to nested classes

--- gsonpath-compiler/src/test/resources/adapter/loader/GeneratedTypeAdapterLoader.java
+++ gsonpath-compiler/src/test/resources/adapter/loader/GeneratedTypeAdapterLoader.java
@@ -4,36 +4,28 @@ import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
 import gsonpath.internal.TypeAdapterLoader;
+
 import java.lang.Override;
-import java.lang.String;
-import java.util.HashMap;
 
 public final class GeneratedTypeAdapterLoader implements TypeAdapterLoader {
-    private final HashMap<String, TypeAdapterLoader> mPackagePrivateLoaders;
+    private final TypeAdapterLoader[] mPackagePrivateLoaders;
 
     public GeneratedTypeAdapterLoader() {
-        mPackagePrivateLoaders = new HashMap<>();
-
-        TypeAdapterLoader adapter_loader_Loader = new adapter.loader.PackagePrivateTypeAdapterLoader();
-        mPackagePrivateLoaders.put("adapter.loader.TestLoaderSource", adapter_loader_Loader);
-
-        TypeAdapterLoader adapter_loader_source3_Loader = new adapter.loader.source3.PackagePrivateTypeAdapterLoader();
-        mPackagePrivateLoaders.put("adapter.loader.source3.TestLoaderSource", adapter_loader_source3_Loader);
-
-        TypeAdapterLoader adapter_loader_source2_Loader = new adapter.loader.source2.PackagePrivateTypeAdapterLoader();
-        mPackagePrivateLoaders.put("adapter.loader.source2.TestLoaderSource", adapter_loader_source2_Loader);
-        mPackagePrivateLoaders.put("adapter.loader.source2.TestLoaderSource2", adapter_loader_source2_Loader);
+        mPackagePrivateLoaders = new TypeAdapterLoader[3];
+        mPackagePrivateLoaders[0] = new adapter.loader.PackagePrivateTypeAdapterLoader();
+        mPackagePrivateLoaders[1] = new adapter.loader.source3.PackagePrivateTypeAdapterLoader();
+        mPackagePrivateLoaders[2] = new adapter.loader.source2.PackagePrivateTypeAdapterLoader();
     }
 
     @Override
     public TypeAdapter create(Gson gson, TypeToken type) {
-        Class rawType = type.getRawType();
+        for (int i = 0; i < mPackagePrivateLoaders.length; i++) {
+            TypeAdapter typeAdapter = mPackagePrivateLoaders[i].create(gson, type);
 
-        TypeAdapterLoader typeAdapterLoader = mPackagePrivateLoaders.get(rawType.getName());
-        if (typeAdapterLoader != null) {
-            return typeAdapterLoader.create(gson, type);
+            if (typeAdapter != null) {
+                return typeAdapter;
+            }
         }
-
         return null;
     }
 }
