68ca5a2fe64d1ebce540d918d34d263b5c7a6491
Lachlan McKee <lachlandevelopment@gmail.com>
2019-04-07 14:35:41 +0100

    Further changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,27 +4,6 @@ buildscript {
     }
 }
 
-ext {
-    var kotlin_version: String by extra
-    var javapoet_version: String by extra
-    var gson_version: String by extra
-    var junit_version: String by extra
-    var project_group: String by extra
-    var core_version: String by extra
-    var compiler_base_version: String by extra
-    var extensions_version: String by extra
-
-    kotlin_version = "1.3.11"
-    javapoet_version = "1.11.1"
-    gson_version = "2.8.1"
-    junit_version = "4.12"
-
-    project_group = "net.lachlanmckee"
-    core_version = "3.1.0"
-    compiler_base_version = "1.3.0"
-    extensions_version = "1.2.0"
-}
-
 plugins {
     id("org.sonarqube") version "2.6.2"
     jacoco
