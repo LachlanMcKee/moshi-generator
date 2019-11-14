4f7ee07b6bda50cd2b405001980c00921fd73d01
Lachlan McKee <lachlandevelopment@gmail.com>
2019-11-14 22:19:33 +0000

    Issue #223 Removed Int/String def support for incremental processing

--- compiler/standard/src/main/java/gsonpath/dependencies/DependencyFactory.kt
+++ compiler/standard/src/main/java/gsonpath/dependencies/DependencyFactory.kt
@@ -9,8 +9,6 @@ import gsonpath.adapter.standard.adapter.read.ReadFunctions
 import gsonpath.adapter.standard.adapter.write.WriteFunctions
 import gsonpath.adapter.standard.extension.ExtensionsHandler
 import gsonpath.adapter.standard.extension.ExtensionsLoader
-import gsonpath.adapter.standard.extension.def.intdef.IntDefExtension
-import gsonpath.adapter.standard.extension.def.stringdef.StringDefExtension
 import gsonpath.adapter.standard.extension.empty.EmptyToNullExtension
 import gsonpath.adapter.standard.extension.flatten.FlattenJsonExtension
 import gsonpath.adapter.standard.extension.invalid.RemoveInvalidElementsExtension
@@ -76,13 +74,9 @@ object DependencyFactory {
         )
     }
 
-    private fun loadExtensions(
-            processingEnv: ProcessingEnvironment): List<GsonPathExtension> {
-
+    private fun loadExtensions(processingEnv: ProcessingEnvironment): List<GsonPathExtension> {
         return ExtensionsLoader.loadExtensions(Logger(processingEnv))
                 .plus(arrayOf(
-                        IntDefExtension(),
-                        StringDefExtension(),
                         EmptyToNullExtension(),
                         FlattenJsonExtension(),
                         RemoveInvalidElementsExtension(),
